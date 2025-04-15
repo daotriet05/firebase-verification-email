c25ed13c0518064b33adf3dd9df721f8d37d1136
Triet Dao <67148915+daotriet05@users.noreply.github.com>
2025-04-15 19:28:25 +0700

    Update firebase-config.js
    remove key

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Replace with your Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyCjFfAuN-DCSTMcmhPaObL4erXrVoadPEw",
-    authDomain: "careerfair2025-user-data.firebaseapp.com",
-    projectId: "careerfair2025-user-data",
-    storageBucket: "careerfair2025-user-data.firebasestorage.app",
-    messagingSenderId: "79335418249",
-    appId: "1:79335418249:web:eacc7b341dcad394b8d958",
-    measurementId: "G-V9QWH9YGBP"
+    
 };
 
 const app = initializeApp(firebaseConfig);
